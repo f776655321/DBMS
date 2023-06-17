2b63faa55e28b3418a11370d3c08f1bc16211570
egnkvn <b09902021@ntu.edu.tw>
2023-06-17 17:14:21 +0800

    Wrap to Match.py

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from Match import CSVMatcher
+from Match import CSVMatcher, DBMatcher
 
 primary_file = 'data/autojoin-Benchmark/texas govs 1/source.csv'
 foreign_file = 'data/autojoin-Benchmark/texas govs 1/target.csv'
+config ={
+  "host": "127.0.0.1",
+  "user": "root",
+  "passwd": "kevin777"
+}
 CSV = CSVMatcher()
-result = CSV.Row_Matcher(True, 'Governor', "Governor's Name", primary_file, foreign_file)
+DB = DBMatcher(config)
+result = CSV.Match(True, primary_file, foreign_file, 'Governor', "Governor's Name")
+print(result)
+result = DB.Match(True, 'Source', 'Target', 'texas_govs_1', 'texas_govs_1', 'Governor', "Governor's_Name")
 print(result)
